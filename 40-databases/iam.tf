1bc17b1792ad0be60ba11126bc709faa9f1e144e
Girisha <girishamunnangi@gmail.com>
2026-03-10 20:11:39 +0530

    vpc

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "mysql" {
-    name = local.myql_role_name
+    name = local.mysql_role_name
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
