6ae0cd4ccc20d4898ef01c5e3b8d62429380c8d6
Girisha <girishamunnangi@gmail.com>
2026-03-10 20:57:29 +0530

    vpc

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -4,5 +4,5 @@ environment=$2
 dnf install ansible -y
 cd /home/ec2-user
 git clone https://github.com/girisha-munnangi/ansible-roboshop-roles-tf.git
-cd ansible-roboshop-roles
+cd ansible-roboshop-roles-tf
 ansible-playbook -e component=$component -e environment=$environment robo.yaml
