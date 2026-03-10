2a4c30aa5da3db227dba9114361bc7ff0ffda2a8
Girisha <girishamunnangi@gmail.com>
2026-03-10 20:39:14 +0530

    vpc

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -18,7 +18,7 @@ filter {
   }
 }
 data "aws_ssm_parameter" "database_subnet_ids" {
-    name = "$/{var.project}/${var.environment}/database_subnet_ids"
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 data "aws_ssm_parameter" "mongodb_sg_id" {
     name = "/${var.project}/${var.environment}/mongodb_sg_id"
