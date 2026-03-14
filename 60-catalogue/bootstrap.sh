8502ff4e55212dc55f74eecc1a6f3e0c851a9691
Girisha <girishamunnangi@gmail.com>
2026-03-14 20:25:13 +0530

    vpc

---
+++ 60-catalogue/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+component=$1
+dnf install ansible -y
+cd /home/ec2-user
+git clone https://github.com/girisha-munnangi/ansible-roboshop-roles-tf.git
+cd ansible-roboshop-roles-tf
+git pull
+ansible-playbook -e component=$component robo.yaml
