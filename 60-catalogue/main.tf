6e91d4427045040cb3b6c29036c6e89443212590
Girisha <girishamunnangi@gmail.com>
2026-03-15 13:50:01 +0530

    vpc

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -72,7 +72,7 @@ resource "aws_launch_template" "catalogue" {
     image_id = aws_ami_from_instance.catalogue.id
     instance_initiated_shutdown_behavior = "terminate"
     instance_type = "t3.micro"
-    vpc_security_group_ids = [local.catalogue.sg_id]
+    vpc_security_group_ids = [local.catalogue_sg_id]
     update_default_version = true
     tags_specifications {
         resource_type = "instance"
