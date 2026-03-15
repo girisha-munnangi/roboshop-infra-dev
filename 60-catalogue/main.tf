298204ca6f5b4f9902f62989355382393410da44
Girisha <girishamunnangi@gmail.com>
2026-03-15 14:31:45 +0530

    vpc

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -99,3 +99,42 @@ resource "aws_launch_template" "catalogue" {
         local.common_tags
     )
 } 
+#create auto scalling groups 
+resource "aws_auto_scaling_group" "catalogue" {
+    name = "${var.project}-${var.environment}-catalogue"
+    max_size = 10
+    min_size = 1
+    health_check_grace_period = 120
+    health_check_type = "ELB"
+    desired_capacity = 1
+    force_delete = false
+    launch_template {
+        id = aws_launch_template.catalogue.id
+        version = "$Latest"
+    }
+    vpc_zone_identifier = [local.private_subnet_id]
+    target_group_arns = [aws_lb_target_group.catalogue.arn]
+    instance_refresh {
+        strategy = "Rolling"
+        preferences {
+            min_healthy_percentage = 50
+        }
+        triggers = ["launch_template"]
+    }
+    dynamic "tag" {
+        for_each = merge(
+            {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+    )
+    content {
+        key = tag.key
+        value = tag.value
+        propogate_at_launch = true
+    }
+    }
+    timeouts {
+        delete = "15m"
+    }
+}
