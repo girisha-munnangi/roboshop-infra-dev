28ff18908a0a67c0f585566303bf6e97bb4b7ade
Girisha <girishamunnangi@gmail.com>
2026-03-23 23:05:29 +0530

    vpc

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "frontend_alb" {
     internal = false
     load_balancer_type = "application"
     security_groups = [local.frontend_alb_sg_id]
-    subnets = local.public_subnet_id
+    subnets = local.public_subnet_ids
     enable_deletion_protection = false
     tags = merge (
         {
