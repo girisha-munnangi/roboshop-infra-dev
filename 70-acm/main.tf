2ffd8bec9eac99064c3d64511a52f8fa33db4235
Girisha <girishamunnangi@gmail.com>
2026-03-23 22:54:52 +0530

    vpc

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -15,7 +15,7 @@ resource "aws_acm_certificate" "roboshop" {
 #creating route53 records
 resource "aws_route53_record" "roboshop" {
     for_each = {
-        for dvo in aws_acm_certificate.roboshop.domain_validation_options:dvo.domain-name => {
+        for dvo in aws_acm_certificate.roboshop.domain_validation_options:dvo.domain_name => {
             name = dvo.resource_record_name
             record = dvo.resource_record_value
             type = dvo.resource_record_type
