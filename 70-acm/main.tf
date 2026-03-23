da2fd0e90cb30635b5c71c7cb3d1e7354aa743cc
Girisha <girishamunnangi@gmail.com>
2026-03-23 22:51:19 +0530

    vpc

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -30,6 +30,6 @@ resource "aws_route53_record" "roboshop" {
 }
 #validating aws acm certificate validation
 resource "aws_acm_certificate_validation" "roboshop" {
-    certificate_arn = aws_acm_certificate.roboshop.certificate.arn
+    certificate_arn = aws_acm_certificate.roboshop.arn
     Validation_record_fqdns = [for record in aws_route53_record.roboshop:record.fqdn]
 }
